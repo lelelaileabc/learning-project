d4f63a1fe615167fea3075e4d14f82f8bc0442df
leon <1463148807@qq.com>
2017-11-26 20:54:07 +0800

    修改描述

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # learning-project
+
+### npm install问题
+1. gyp ERR! stack Error: Can't find Python executable "python", you can set the PYTHON env variable.
+
+    原因看node-gyp的安装需求
+    >Install Python 2.7 (v3.x.x is not supported), and run npm config set python python2.7 (or see below for further instructions on specifying the proper Python version and path.)
+    >
+    >https://github.com/nodejs/node-gyp
+
+    下载安装python2.7 https://www.python.org/downloads/
+    
+1. Node Sass could not find a binding for your current environment: Windows 64-bit with Node.js 8.x
+
+    执行
+    npm rebuild node-sass
--- gulpfile.js
+++ gulpfile.js
@@ -13,50 +13,51 @@ const concat = require('gulp-concat');
 */
 
 // 调用方法打印
-gulp.task('message', function(){
-  return console.log('Gulp is running...');
+gulp.task('message', function () {
+    return console.log('Gulp is running...');
 });
 
 // 拷贝所有的html文件
-gulp.task('copyHtml', function(){
-  gulp.src('src/*.html')
-      .pipe(gulp.dest('dist'));
+gulp.task('copyHtml', function () {
+    gulp.src('src/*.html')
+        .pipe(gulp.dest('dist'));
 });
 
 // 压缩图片
-gulp.task('imageMin', () =>
-	gulp.src('src/images/*')
-		.pipe(imagemin())
-		.pipe(gulp.dest('dist/images'))
+gulp.task('imageMin', function () {
+        gulp.src('src/images/*')
+            .pipe(imagemin())
+            .pipe(gulp.dest('dist/images'));
+    }
 );
 
 // 压缩js
-gulp.task('minify', function(){
-  gulp.src('src/js/*.js')
-      .pipe(uglify())
-      .pipe(gulp.dest('dist/js'));
+gulp.task('minify', function () {
+    gulp.src('src/js/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
 });
 
 // 合并scss
-gulp.task('sass', function(){
-  gulp.src('src/sass/*.scss')
-      .pipe(sass().on('error', sass.logError))
-      .pipe(gulp.dest('dist/css'));
+gulp.task('sass', function () {
+    gulp.src('src/sass/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('dist/css'));
 });
 
 // Scripts
-gulp.task('scripts', function(){
-  gulp.src('src/js/*.js')
-      .pipe(concat('main.js'))
-      .pipe(uglify())
-      .pipe(gulp.dest('dist/js'));
+gulp.task('scripts', function () {
+    gulp.src('src/js/*.js')
+        .pipe(concat('main.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('default', ['message', 'copyHtml', 'imageMin', 'sass', 'scripts']);
 
-gulp.task('watch', function(){
-  gulp.watch('src/js/*.js', ['scripts']);
-  gulp.watch('src/images/*', ['imageMin']);
-  gulp.watch('src/sass/*.scss', ['sass']);
-  gulp.watch('src/*.html', ['copyHtml']);
+gulp.task('watch', function () {
+    gulp.watch('src/js/*.js', ['scripts']);
+    gulp.watch('src/images/*', ['imageMin']);
+    gulp.watch('src/sass/*.scss', ['sass']);
+    gulp.watch('src/*.html', ['copyHtml']);
 });
